39445725396c769e318cd34f2b317ae7227547c2
AntonKralin <antonkralin91@gmail.com>
2023-04-25 21:31:51 +0300

    add test autorizate

--- dj/uktio/views.py
+++ dj/uktio/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import *
 
 
 # Create your views here.
 def index(request:HttpRequest):
-    return render(request, 'uktio/index.html')    
+    if request.method == "POST":
+        user_name = request.session.get('user', None)
+        user_pass = request.session.get("password", None)
+        print(user_name, user_pass)
+        if user_name and user_pass:
+               user_qset = Users.objects.filter(login=user_name, password=user_pass)
+               if user_qset.exists():
+                   user = user_qset.first()
+                   request.session['user'] = user
+                   return render(request, 'uktio/index.html')
+        return render(request, 'uktio/index.html', context=
+                        {'message': 'Wrong user or password'})
+    else:
+        return render(request, 'uktio/index.html')
 
 def main(request:HttpRequest):
     return render(request, "uktio/main.html")
